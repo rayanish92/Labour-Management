e1d076cf8c8331153ae17bdb393af094a5d1f18b
rayanish92 <rayanish92@gmail.com>
2026-04-23 17:05:53 +0530

    Create main.py

---
+++ server/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes import router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/health")
+def health():
+    return {"status": "API running"}
+
+app.include_router(router, prefix="/api")
